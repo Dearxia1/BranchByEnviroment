503f7e9c153ce8921f1fe39593e5d2b3fc861c6e
Test User <test@example.com>
2026-02-23 15:54:02 -0500

    feat: actualizar lógica a versión 2.0

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 const config = require('./config.json');
-console.log(`Versión 1.0 - Corriendo en entorno: ${config.env}`);
-console.log(`Conectando a API: ${config.api}`);
+console.log(`Versión 2.0 - Corriendo en entorno: ${config.env}`);
+console.log(`Conectando a nueva API v2: ${config.api}`);
